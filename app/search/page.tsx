4878901ed61266daab042fd807e4cd30e5c5ad18
rnn2001 <romels064@gmail.com>
2025-03-28 21:13:48 +0100

    final

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,25 +1,22 @@
-import getSongsByTitle from "@/actions/getSongsByTitle"
-import Header from "@/components/Header"
-import SearchInput from "@/components/SearchInput"
-import SearchContent from "./components/SearchContent"
+import getSongsByTitle from "@/actions/getSongsByTitle";
+import Header from "@/components/Header";
+import SearchInput from "@/components/SearchInput";
+import SearchContent from "./components/SearchContent";
 
- 
- interface SearchProps {
-    searchParams:{
-        title?: string
-    }
- }
+interface SearchProps {
+  searchParams: Promise<{ title: string }>;
+}
 
 export const revalidate = 0;
 
- const Search = async ({ searchParams }: SearchProps) => {
-    const songs = searchParams.title
-      ? await getSongsByTitle(searchParams.title)
-      : [];
+const Search = async ({ searchParams }: SearchProps) => {
+  const { title } = await searchParams;
+  const songs = await getSongsByTitle(title);
+  
 
-    return (
-      <div
-        className="
+  return (
+    <div
+      className="
             bg-neutral-900
             rounded-lg
             h-full
@@ -27,16 +24,16 @@ export const revalidate = 0;
             overflow-hidden
             overflow-y-auto
           "
-      >
-        <Header className="from-bg-neutral-900">
-          <div className="mb-2 flex flex-col gap-y-6">
-            <h1 className="text-white text-3xl fon-semiblod">Search</h1>
-            <SearchInput />
-          </div>
-        </Header>
-        <SearchContent songs={songs} />
-      </div>
-    );
- }
+    >
+      <Header className="from-bg-neutral-900">
+        <div className="mb-2 flex flex-col gap-y-6">
+          <h1 className="text-white text-3xl fon-semiblod">Search</h1>
+          <SearchInput />
+        </div>
+      </Header>
+      <SearchContent songs={songs} />
+    </div>
+  );
+};
 
- export default Search
+export default Search;
